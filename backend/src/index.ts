765b6ce0b7e74cf16c643950b2e5a91af0eb7b22
kryniax <kryniax@gmail.com>
2024-11-21 00:44:15 +0100

    first commit

---
+++ backend/src/index.ts
@@ -0,0 +1,21 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import "dotenv/config";
+import mongoose from "mongoose";
+
+mongoose
+  .connect(process.env.MONGODB_CONNECTION_STRING as string)
+  .then(() => console.log("Connected to database"));
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.get("/", async (req: Request, res: Response) => {
+  res.json({ message: "working app" });
+});
+
+app.listen(8000, () => {
+  console.log("server running on localhost:8000");
+});
