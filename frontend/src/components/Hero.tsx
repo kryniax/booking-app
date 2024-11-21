fcf751aba2e50bb9d442b553586161300147aac4
kryniax <kryniax@gmail.com>
2024-11-21 13:51:03 +0100

    add hero section

---
+++ frontend/src/components/Hero.tsx
@@ -0,0 +1,18 @@
+const Hero = () => {
+  return (
+    <div className="bg-blue-800 pb-16">
+      <div className="container mx-auto flex flex-col gap-2">
+        <header>
+          <h1 className="text-3xl md:text-5xl text-white font-bold">
+            Find your next stay
+          </h1>
+        </header>
+        <p className="text-xl md:text-2xl text-white">
+          Search low prices on hotels for your dream holidays.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,6 +4,12 @@ export default {
   theme: {
     container: {
       center: true,
+      padding: {
+        default: "1rem",
+        sm: "1rem",
+        md: "5rem",
+        lg: "10rem",
+      },
     },
     screens: {
       sm: "375px",
