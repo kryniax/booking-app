20de51a282f65fa4f30099c730b65a3e69891cdc
kryniax <kryniax@gmail.com>
2024-11-21 14:25:25 +0100

    app route update

--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -1,13 +1,7 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Layout from "./layouts/Layout";
 
 function AppRoutes() {
-  const [count, setCount] = useState(0);
-
   return (
     <Routes>
       <Route
