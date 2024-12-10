f0db48f96d3635a2c1390b434ccaae50444f8705
kryniax <kryniax@gmail.com>
2024-12-10 16:27:49 +0100

    add getMyHotels request

--- frontend/src/api/HotelApi.tsx
+++ frontend/src/api/HotelApi.tsx
@@ -1,14 +1,17 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import { HotelFormData } from "../forms/manage-hotel-form/ManageHotelForm";
 import { useAppContext } from "../contexts/AppContext";
 import { useNavigate } from "react-router-dom";
+import { HotelType } from "../types";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const useCreateMyHotel = () => {
   const { showToast } = useAppContext();
   const navigate = useNavigate();
-  const createMyHotelRequest = async (hotelFormData: FormData) => {
+  const createMyHotelRequest = async (
+    hotelFormData: FormData
+  ): Promise<HotelType> => {
     const response = await fetch(`${API_BASE_URL}/api/my/hotel`, {
       method: "POST",
       credentials: "include",
@@ -37,6 +40,31 @@ export const useCreateMyHotel = () => {
     createMyHotel,
     isError: status === "error",
     isSuccess: status === "success",
+    isLoading: status === "pending",
+  };
+};
+
+export const useGetMyHotels = () => {
+  const getMyHotelsRequest = async (): Promise<HotelType[]> => {
+    const response = await fetch(`${API_BASE_URL}/api/my/hotel`, {
+      credentials: "include",
+    });
+
+    if (!response.ok) {
+      throw new Error("Error fetching hotels");
+    }
+
+    return response.json();
+  };
+
+  const { data: myHotels, status } = useQuery({
+    queryKey: ["fetchMyHotels"],
+    queryFn: getMyHotelsRequest,
+    retry: false,
+  });
+
+  return {
+    myHotels,
     isLoading: status === "pending",
   };
 };
