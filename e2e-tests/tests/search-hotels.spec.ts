374f0506fca6c01c15983fdaaa1dd52b62465c0f
kryniax <kryniax@gmail.com>
2024-12-15 08:32:28 +0100

    add show hotel details test

--- e2e-tests/tests/search-hotels.spec.ts
+++ e2e-tests/tests/search-hotels.spec.ts
@@ -27,3 +27,17 @@ test("should show hotel search results", async ({ page }) => {
   await expect(page.getByText("Hotels found in Test City")).toBeVisible();
   await expect(page.getByText("TestHotel").first()).toBeVisible();
 });
+
+test("should show hotel detail", async ({ page }) => {
+  await page.goto(UI_URL);
+
+  await page.getByPlaceholder("Where are you going?").fill("Test City");
+  await page.getByRole("button", { name: "Search" }).click();
+
+  await expect(page.getByText("TestHotel").first()).toBeVisible();
+
+  await page.getByRole("link", { name: "View More" }).first().click();
+
+  await expect(page).toHaveURL(/detail/);
+  await expect(page.getByRole("button", { name: "Book now" })).toBeVisible();
+});
