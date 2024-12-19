ef339b4fb6316d60bc148a522f0bc2232466ff63
kryniax <kryniax@gmail.com>
2024-12-19 20:38:35 +0100

    add i18n package

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { AppContextProvider } from "./contexts/AppContext.tsx";
 import { SearchContextProvider } from "./contexts/SearchContext.tsx";
+import "./config/i18n";
 
 const queryClient = new QueryClient({
   defaultOptions: {
