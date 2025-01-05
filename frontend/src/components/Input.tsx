a8432ad5051856718cba83275e0be09186f2f2e2
kryniax <kryniax@gmail.com>
2025-01-05 22:24:26 +0100

    update input component

--- frontend/src/components/Input.tsx
+++ frontend/src/components/Input.tsx
@@ -1,4 +1,4 @@
-import { forwardRef, HTMLProps, Ref } from "react";
+import React, { forwardRef, HTMLProps, Ref } from "react";
 import { FieldError } from "react-hook-form";
 import { twMerge } from "tailwind-merge";
 
@@ -6,10 +6,12 @@ interface InputProps extends HTMLProps<HTMLInputElement> {
   label: string;
   labelClass?: string;
   error?: FieldError;
+  children?: React.ReactNode;
 }
 
 const Input = forwardRef((props: InputProps, ref: Ref<HTMLInputElement>) => {
-  const { label, labelClass, error, className, ...inputProps } = props;
+  const { label, labelClass, error, className, children, ...inputProps } =
+    props;
   return (
     <label
       className={twMerge(
@@ -18,14 +20,19 @@ const Input = forwardRef((props: InputProps, ref: Ref<HTMLInputElement>) => {
       )}
     >
       <span className="capitalize">{label}</span>
-      <input
-        {...inputProps}
-        ref={ref}
-        className={twMerge(
-          `border rounded w-full py-1 px-2 font-normal dark:bg-zinc-800 dark:border-zinc-700`,
-          className
-        )}
-      />
+      {children ? (
+        children
+      ) : (
+        <input
+          {...inputProps}
+          ref={ref}
+          className={twMerge(
+            `border rounded w-full py-1 px-2 font-normal dark:bg-zinc-800 dark:border-zinc-700`,
+            className
+          )}
+        />
+      )}
+
       {error && (
         <span className="text-red-500 text-sm font-bold">{error.message}</span>
       )}
