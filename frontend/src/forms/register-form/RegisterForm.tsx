2fa0e08ee35c7fdbf308fe6713543b500eaf61d4
kryniax <kryniax@gmail.com>
2025-01-05 22:10:21 +0100

    add register form component

---
+++ frontend/src/forms/register-form/RegisterForm.tsx
@@ -0,0 +1,133 @@
+import { TFunction } from "i18next";
+import React, { useMemo } from "react";
+import { z } from "zod";
+import HelmetSEO from "../../components/HelmetSEO";
+import Input from "../../components/Input";
+import { useTranslation } from "react-i18next";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useCreateUser } from "../../api/UserApi";
+import { Link } from "react-router-dom";
+
+const registerFormSchema = (t: TFunction) =>
+  z
+    .object({
+      firstName: z
+        .string()
+        .min(3, { message: t("RegisterPage.validation.firstName.message") }),
+      lastName: z
+        .string()
+        .min(3, { message: t("RegisterPage.validation.lastName.message") }),
+      email: z
+        .string()
+        .email({ message: t("RegisterPage.validation.email.message") }),
+      password: z
+        .string()
+        .min(8, t("RegisterPage.validation.password.min"))
+        .max(128, t("RegisterPage.validation.password.max"))
+        .refine(
+          (value) => /[a-z]/.test(value),
+          t("RegisterPage.validation.password.lowerCase")
+        )
+        .refine(
+          (value) => /[A-Z]/.test(value),
+          t("RegisterPage.validation.password.upperCase")
+        )
+        .refine(
+          (value) => /[0-9]/.test(value),
+          t("RegisterPage.validation.password.number")
+        )
+        .refine(
+          (value) => /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>/?]/.test(value),
+          t("RegisterPage.validation.password.special")
+        ),
+      confirmPassword: z.string(),
+    })
+    .refine((data) => data.password === data.confirmPassword, {
+      message: t("RegisterPage.validation.confirmPassword.message"),
+      path: ["confirmPassword"],
+    });
+
+export type RegisterFormData = z.infer<ReturnType<typeof registerFormSchema>>;
+
+const RegisterForm = () => {
+  const { t } = useTranslation();
+  const formSchema = useMemo(() => {
+    return registerFormSchema(t);
+  }, [t]);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<RegisterFormData>({
+    resolver: zodResolver(formSchema),
+  });
+
+  const { createUser } = useCreateUser();
+
+  const onSubmit = handleSubmit(async (data) => {
+    try {
+      await createUser(data);
+    } catch (error) {
+      console.error("Registration failed: ", error);
+    }
+  });
+  return (
+    <form className="flex flex-col gap-5" onSubmit={onSubmit}>
+      <h2 className="text-3xl font-bold dark:text-zinc-100">
+        {t("RegisterPage.title")}
+      </h2>
+      <div className="flex flex-col md:flex-row gap-5">
+        <Input
+          label={t("RegisterPage.validation.firstName.label")}
+          type="text"
+          error={errors.firstName}
+          {...register("firstName")}
+        />
+        <Input
+          label={t("RegisterPage.validation.lastName.label")}
+          type="text"
+          error={errors.lastName}
+          {...register("lastName")}
+        />
+      </div>
+      <Input
+        label={t("RegisterPage.validation.email.label")}
+        type="email"
+        error={errors.email}
+        {...register("email")}
+      />
+      <Input
+        label={t("RegisterPage.validation.password.label")}
+        type="password"
+        error={errors.password}
+        {...register("password")}
+      />
+      <Input
+        label={t("RegisterPage.validation.confirmPassword.label")}
+        type="password"
+        error={errors.confirmPassword}
+        {...register("confirmPassword")}
+      />
+      <div className="flex items-center justify-between">
+        <span className="text-sm dark:text-zinc-100">
+          {t("RegisterPage.alreadyAccount")}{" "}
+          <Link
+            className="underline hover:text-black/80 transition duration-100"
+            to="/login"
+          >
+            {t("BookingApp.signIn")}
+          </Link>
+        </span>
+        <button
+          type="submit"
+          className="bg-blue-600 dark:bg-blue-900 text-white text-xl p-2 font-bold rounded-md hover:bg-blue-500 dark:hover:bg-blue-800 transition duration-100"
+        >
+          {t("RegisterPage.button")}
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default RegisterForm;
--- frontend/src/pages/RegisterPage.tsx
+++ frontend/src/pages/RegisterPage.tsx
@@ -1,134 +1,18 @@
-import { useForm } from "react-hook-form";
-import { z } from "zod";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useCreateUser } from "../api/UserApi";
-import { TFunction } from "i18next";
-import { useTranslation } from "react-i18next";
-import { useMemo } from "react";
-import { Link } from "react-router-dom";
-import Input from "../components/Input";
 import HelmetSEO from "../components/HelmetSEO";
-
-const registerFormSchema = (t: TFunction) =>
-  z
-    .object({
-      firstName: z
-        .string()
-        .min(3, { message: t("RegisterPage.validation.firstName.message") }),
-      lastName: z
-        .string()
-        .min(3, { message: t("RegisterPage.validation.lastName.message") }),
-      email: z
-        .string()
-        .email({ message: t("RegisterPage.validation.email.message") }),
-      password: z
-        .string()
-        .min(8, t("RegisterPage.validation.password.min"))
-        .refine(
-          (value) => /[A-Z]/.test(value),
-          t("RegisterPage.validation.password.upperCase")
-        )
-        .refine(
-          (value) => /[0-9]/.test(value),
-          t("RegisterPage.validation.password.number")
-        )
-        .refine(
-          (value) => /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>/?]/.test(value),
-          t("RegisterPage.validation.password.special")
-        ),
-      confirmPassword: z.string(),
-    })
-    .refine((data) => data.password === data.confirmPassword, {
-      message: t("RegisterPage.validation.confirmPassword.message"),
-      path: ["confirmPassword"],
-    });
-
-export type RegisterFormData = z.infer<ReturnType<typeof registerFormSchema>>;
+import RegisterForm from "../forms/register-form/RegisterForm";
 
 const RegisterPage = () => {
-  const { t } = useTranslation();
-  const formSchema = useMemo(() => {
-    return registerFormSchema(t);
-  }, [t]);
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<RegisterFormData>({
-    resolver: zodResolver(formSchema),
-  });
-
-  const { createUser } = useCreateUser();
-
-  const onSubmit = handleSubmit(async (data) => {
-    try {
-      await createUser(data);
-    } catch (error) {
-      console.error("Registration failed: ", error);
-    }
-  });
-
   return (
-    <form className="flex flex-col gap-5" onSubmit={onSubmit}>
+    <>
+      {" "}
       <HelmetSEO
         title="Create Hotel Booking Account | Free Registration"
         description="Sign up for a free account to manage bookings and get access to exclusive hotel deals and promotions."
         keywords="hotel registration, create account, sign up, booking account, free registration"
         pathName="/register"
       />
-      <h2 className="text-3xl font-bold dark:text-zinc-100">
-        {t("RegisterPage.title")}
-      </h2>
-      <div className="flex flex-col md:flex-row gap-5">
-        <Input
-          label={t("RegisterPage.validation.firstName.label")}
-          type="text"
-          error={errors.firstName}
-          {...register("firstName")}
-        />
-        <Input
-          label={t("RegisterPage.validation.lastName.label")}
-          type="text"
-          error={errors.lastName}
-          {...register("lastName")}
-        />
-      </div>
-      <Input
-        label={t("RegisterPage.validation.email.label")}
-        type="email"
-        error={errors.email}
-        {...register("email")}
-      />
-      <Input
-        label={t("RegisterPage.validation.password.label")}
-        type="password"
-        error={errors.password}
-        {...register("password")}
-      />
-      <Input
-        label={t("RegisterPage.validation.confirmPassword.label")}
-        type="password"
-        error={errors.confirmPassword}
-        {...register("confirmPassword")}
-      />
-      <div className="flex items-center justify-between">
-        <span className="text-sm dark:text-zinc-100">
-          {t("RegisterPage.alreadyAccount")}{" "}
-          <Link
-            className="underline hover:text-black/80 transition duration-100"
-            to="/login"
-          >
-            {t("BookingApp.signIn")}
-          </Link>
-        </span>
-        <button
-          type="submit"
-          className="bg-blue-600 dark:bg-blue-900 text-white text-xl p-2 font-bold rounded-md hover:bg-blue-500 dark:hover:bg-blue-800 transition duration-100"
-        >
-          {t("RegisterPage.button")}
-        </button>
-      </div>
-    </form>
+      <RegisterForm />
+    </>
   );
 };
 
